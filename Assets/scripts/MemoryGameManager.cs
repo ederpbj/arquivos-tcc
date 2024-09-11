3ad6c219b37ed77f3ff4a236db6214296835ba04
Éder Pires <ederpbj@gmail.com>
2024-09-10 23:27:16 -0300

    ok

--- Assets/scripts/MemoryGameManager.cs
+++ Assets/scripts/MemoryGameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Importante para trabalhar com UI
 
 public class MemoryGameManager : MonoBehaviour
 {
     public GameObject[] cards; // Array para as cartas
     public Transform[] cardPositions; // Posições onde as cartas serão colocadas
+    public Button shuffleButton; // Referência ao botão de embaralhar
 
     void Start()
     {
         ShuffleCards();
         PlaceCards();
+
+        // Associar a função ShuffleAndPlace ao clique do botão
+        shuffleButton.onClick.AddListener(ShuffleAndPlace);
     }
 
     // Função para embaralhar as cartas usando Fisher-Yates Shuffle
@@ -33,5 +38,12 @@ public class MemoryGameManager : MonoBehaviour
             // Posiciona cada carta em uma das posições predefinidas
             cards[i].transform.position = cardPositions[i].position;
         }
+    }
+
+    // Função chamada ao clicar no botão para embaralhar e reposicionar as cartas
+    public void ShuffleAndPlace()
+    {
+        ShuffleCards();
+        PlaceCards();
     }
 }
